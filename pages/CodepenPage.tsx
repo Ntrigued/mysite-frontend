b8466e895e69f9f3b1f52669b7fe17ca13751e08
Ntrigued <virtfund@gmail.com>
2024-03-29 02:31:53 +0000

    Add a coepen

---
+++ pages/CodepenPage.tsx
@@ -0,0 +1,38 @@
+import Script from "next/script";
+
+export default function CodepenPage(params: any) {
+  return (
+    <div style={{ marginLeft: "5%", marginTop: "2.5%" }}>
+      <p
+        class="codepen"
+        data-height="300"
+        data-theme-id="dark"
+        data-default-tab="result"
+        data-slug-hash="VwgowMX"
+        data-user="allbeematthew"
+        style={{
+          height: "50vh",
+          boxSizing: "border-box",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          border: "2px solid",
+          margin: "1em 0",
+          padding: "1em",
+        }}
+      >
+        <span>
+          See the Pen{" "}
+          <a href="https://codepen.io/allbeematthew/pen/VwgowMX">2048 game</a>{" "}
+          by Matthew Allbee (
+          <a href="https://codepen.io/allbeematthew">@allbeematthew</a>) on{" "}
+          <a href="https://codepen.io">CodePen</a>.
+        </span>
+      </p>
+      <Script
+        src="https://cpwebassets.codepen.io/assets/embed/ei.js"
+        defer={false}
+      ></Script>
+    </div>
+  );
+}
